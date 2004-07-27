24e7be94e97ed727f185cfffec153aa626511990
☺唐鳳☻ <cpan@audreyt.org>
2004-07-27 23:52:00 +0000

    Import of AUTRIJUS/Kwiki-Archive-SVK-0.03 from CPAN.
    gitpan-cpan-distribution: Kwiki-Archive-SVK
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AUTRIJUS/Kwiki-Archive-SVK-0.03.tar.gz
    gitpan-cpan-author:       AUTRIJUS
    gitpan-cpan-maturity:     released

--- lib/Kwiki/Archive/SVK.pm
+++ lib/Kwiki/Archive/SVK.pm
@@ -4,8 +4,13 @@ use warnings;
 use SVK;
 use SVK::XD;
 use SVN::Repos;
+use File::Glob;
+use Time::Local;
 use Kwiki::Archive '-Base';
-our $VERSION = '0.01';
+our $VERSION = '0.03';
+
+field 'svk_cache_obj' => -1;
+field 'svk_cache_handle' => undef;
 
 sub register {
     super;
@@ -19,6 +24,10 @@ sub register {
 
 sub generate {
     super;
+
+    my $rcs_dump = $self->export_rcs;
+    $_->unlink for io($self->plugin_directory)->all;
+
     SVN::Repos::create(
         $self->plugin_directory, undef, undef, undef, {
             ($SVN::Core::VERSION =~ /^1\.0/) ? (
@@ -29,133 +38,271 @@ sub generate {
             )
         }
     );
+
+    $self->import_rcs($rcs_dump) if $rcs_dump;
+}
+
+sub import_rcs {
+    my $rcs_dump = shift;
+    my $page = $self->hub->pages->page_class->new($self->hub);
+    my $meta = $self->hub->pages->meta_class->new($self->hub);
+
+    foreach my $id (sort keys %$rcs_dump) {
+        local $SIG{__WARN__} = sub { 1 };
+        print STDERR "Storing $id";
+        my $history = $rcs_dump->{$id};
+        $page->id($id);
+        $meta->id($id);
+        foreach my $info (reverse @$history) {
+            print STDERR ".";
+            $page->content(delete $info->{content});
+            $meta->from_hash($info);
+            $page->metadata($meta);
+            $page->store;
+        }
+        print STDERR "\n";
+    }
+}
+
+sub export_rcs {
+    my @files = File::Glob::bsd_glob(
+        io->catfile($self->plugin_directory, '*,v')->absolute
+    ) or return;
+
+    require Kwiki::Archive::Rcs;
+    my $rcs = Kwiki::Archive::Rcs->new;
+    my $page = $self->hub->pages->page_class->new($self->hub);
+
+    return {
+        map {
+            print STDERR "Loading $_...\n";
+            $page->id($_);
+            my $history = $rcs->history($page);
+            $_->{content} = $rcs->fetch($page, delete $_->{revision_id})
+              foreach @$history;
+            ($page->id => $history);
+        } map {
+            m{([^\\/]+),v$} ? $1 : ()
+        } @files
+    }
+}
+
+sub empty {
+    not io->catfile($self->plugin_directory, 'db', 'fs-type')->exists;
 }
 
 sub attachments_upload {
     my ($attachments, $page_id, $file, $message) = @_;
-    my ($svk, $out) = $self->svk($attachments);
+
     my $co_file = io->catfile(
-        io->rel2abs($attachments->plugin_directory), $page_id, $file
-    )->name;
-    $svk->mkdir(-m => "", "//attachments/$page_id");
-    $svk->add($co_file);
-    $svk->commit(-m => "$message", $co_file);
+        $attachments->plugin_directory, $page_id, $file
+    )->absolute;
+
+    $self->svk(
+        $attachments,
+        mkdir   => [ -m => "", "//attachments/$page_id" ],
+        add     => [ $co_file ],
+        commit  => [ -m => "$message", $co_file ]
+    );
 }
 
 sub attachments_list {
     my ($attachments, $page_id) = @_;
-    my ($svk, $out) = $self->svk($attachments);
-    $svk->list("//attachments/$page_id");
-    foreach my $file (split(/\n/, $$out)) {
-        $svk->revert(
-            io->catfile(
-                io->rel2abs($attachments->plugin_directory), $page_id, $file
-            )->name
-        );
-    }
+
+    my $out = $self->svk(
+        $attachments,
+        list => [ "//attachments/$page_id" ],
+    );
+
+    $self->svk(
+        $attachments,
+        map (
+            (revert => [ 
+                io->catfile(
+                    $attachments->plugin_directory,
+                    $page_id,
+                    $_,
+                )->absolute
+            ]),
+            split(/\n/, $out)
+        ),
+    );
 }
 
 sub attachments_delete {
     my ($attachments, $page_id, $file, $message) = @_;
-    my ($svk, $out) = $self->svk($attachments);
     my $co_file = io->catfile(
-        io->rel2abs($attachments->plugin_directory), $page_id, $file
-    )->name;
-    $svk->delete($co_file);
-    $svk->commit(-m => "$message", $co_file);
+        $attachments->plugin_directory, $page_id, $file
+    )->absolute;
+
+    $self->svk(
+        $attachments,
+        delete => [ $co_file ],
+        commit => [ -m => "$message", $co_file ],
+    );
 }
 
 sub page_content {
     my $page = shift;
-    my ($svk, $out) = $self->svk($page);
-    my $co_file = $page->io->rel2abs;
+    my $co_file = $page->io->absolute;
 
-    my ($atime, $mtime) = (stat $co_file)[8, 9];
-    $svk->revert($co_file);
-    utime($atime, $mtime, $co_file);
+    my ($atime, $mtime) = ($co_file->stat)[8, 9];
+    $self->svk( $page, up  => [ $co_file ] );
+    $self->svk( $page, revert  => [ $co_file ] );
+    utime($atime, $mtime, $co_file) if $mtime;
 }
 
 sub page_metadata {
     my $page = shift;
-    my ($svk, $out) = $self->svk($page);
-    my $co_file = $page->io->rel2abs;
+    return;
+
     my $metadata = $page->{metadata};
 
-    $svk->proplist($co_file); utf8::decode($$out);
-    $metadata->from_hash({ $$out =~ /^kwiki:(\w+): (.+)$/mg });
+    $metadata->from_hash($self->fetch_metadata($page));
     $metadata->store;
 }
 
 sub commit {
-    my $page = shift;
-    my $message = shift;
-    my ($svk, $out) = $self->svk($page);
-    my $co_file = $page->io->rel2abs;
+    my ($page, $message) = @_;
+    my $co_file = $page->io->absolute;
     my $props = $self->page_properties($page);
 
-    $svk->add($co_file);
-    $svk->propset("kwiki:$_" => $props->{$_}, $co_file)
-      foreach sort keys %$props;
-    $svk->commit(-m => "$message", $co_file);
+    local $ENV{USER} = $props->{edit_by} || $self->user_name;
+    $message = '' if not defined $message;
+
+    # XXX - what about $props->{edit_time}?
+
+    $self->svk(
+        $page, 
+        add     => [ $co_file ],
+        commit  => [ -m => "$message", $co_file ],
+    );
+}
+
+sub revision_numbers {
+    my $page = shift;
+    my $limit = shift;
+
+    my $handle = $self->svk_handle($page);
+    my $fs = ($handle->{xd}->find_repos('//', 1))[2]->fs;
+    my $path = "/pages/".$page->id;
+
+    my $pool = SVN::Pool->new_default;
+    my $hist = $fs->revision_root($fs->youngest_rev)->node_history($path);
+    my @rv;
+
+    $limit = -1 if !$limit;
+    while (($hist = $hist->prev(0)) and $limit--) {
+        push @rv, ($hist->location)[1];
+        $pool->clear;
+    }
+
+    return \@rv;
+}
+
+sub fetch_metadata {
+    my ($page, $rev) = @_;
+    my $co_file = $page->io->absolute;
+
+    $self->svk(
+        $page,
+        log => [ ($rev ? ( -r => $rev ) : ( -l => 1 )), $co_file ]
+    ) =~ /r(\d+): +(.*) \| (.+)\n\n([\d\D]+)\n/ or return {};
+
+    return {
+        revision_id     => $1,
+        edit_by         => $2,
+        message         => $4,
+        $self->timestamp_props($3),
+    };
+}
+
+sub timestamp_props {
+    my $time = shift;
+
+    $time =~ /(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)/ or return;
+    my $gmtime = timegm($6, $5, $4, $3, $2, $1);
+
+    return (
+        edit_time       => scalar gmtime($gmtime),
+        edit_unixtime   => $gmtime,
+    );
 }
 
 sub history {
     my $page = shift;
-    my ($svk, $out) = $self->svk($page);
-    my $co_file = $page->io->rel2abs;
-    $svk->log($co_file); utf8::decode($$out);
 
-    return [map {
-        my ($rev, $msg) = /r(\d+):.*\n\n([\d\D]*)/;
-        $rev ? do {
-            $svk->proplist(-r => $rev, $co_file);
-            +{
-                $$out =~ /^kwiki:(\w+): (.+)$/mg,
-                message => $msg,
-                revision_id => $rev,
-            };
-        } : ();
-    } split /^-+\n/m, $$out];
+    return [
+        map $self->fetch_metadata($page, $_),
+            @{$self->revision_numbers($page)}
+    ];
 }
 
 sub fetch {
-    my $page = shift;
-    my $revision_id = shift;
-    my ($svk, $out) = $self->svk($page);
+    my ($page, $revision_id) = @_;
 
-    $svk->cat(-r => $revision_id, $page->io->rel2abs);
-    $self->utf8_decode($$out);
+    return $self->svk(
+        $page,
+        cat => [ -r => $revision_id, $page->io->absolute ],
+    );
 }
 
 sub svk {
     my $obj = shift;
-    my $co = Data::Hierarchy->new;
+
+    local @ENV{qw(SVKMERGE SVKDIFF LC_CTYPE LC_ALL LANG LC_MESSAGES)};
+    local *SVK::I18N::loc = *SVK::I18N::_default_gettext;
+
+    my $svk = $self->svk_handle($obj);
+
+    while (my $cmd = shift) {
+        my $args = shift;
+        $svk->$cmd(map "$_", @$args);
+    }
+
+    return unless defined wantarray;
+    return $self->utf8_decode(${$svk->{output}});
+}
+
+sub svk_handle {
+    my $obj = shift;
+    return $obj->{svk_handle} if $obj->{svk_handle};
+
+    my $co_obj = Data::Hierarchy->new;
+    my $co_path = $self->plugin_directory;
+
     my $xd = SVK::XD->new(
-        depotmap => { '' => io->rel2abs($self->plugin_directory) },
-        checkout => $co,
-        svkpath => io->rel2abs($self->plugin_directory),
+        depotmap => { '' => $co_path },
+        checkout => $co_obj,
+        svkpath  => $co_path,
     );
 
-    my $output;
     my $repos = ($xd->find_repos('//', 1))[2];
-    my $svk = SVK->new(xd => $xd, output => \$output);
+    my $svk = SVK->new(xd => $xd, output => \(my $output));
 
-    if ($obj->class_id eq 'page') {
-        $co->store(
-            io->rel2abs($obj->database_directory),
-            { depotpath => '//pages', revision => $repos->fs->youngest_rev },
-        );
-        $svk->mkdir(-m => "", "//pages");
-    }
-    elsif ($obj->class_id eq 'attachments') {
-        $co->store(
-            io->rel2abs($obj->plugin_directory),
-            { depotpath => '//attachments', revision => $repos->fs->youngest_rev },
-        );
-        $svk->mkdir(-m => "", "//attachments");
+    my $subdir = $obj->class_id;
+    $subdir =~ s/s?$/s/; # pluralize the directory name
+
+    my $method = {
+        pages => 'database_directory',
+    }->{$subdir} || 'plugin_directory';
+
+    $co_obj->store(
+        io($obj->$method)->absolute,
+        { depotpath => "//$subdir", revision => $repos->fs->youngest_rev },
+    );
+
+    # mkdir $subdir if not exists -- refactor back to SVK!
+    my $fs = ($svk->{xd}->find_repos('//', 1))[2]->fs;
+    my $root = $fs->revision_root($fs->youngest_rev);
+    if ($root->check_path("/$subdir") == $SVN::Node::none) {
+        my $pool = SVN::Pool->new_default;
+        $root->make_dir("/$subdir", $pool);
+        $pool->clear;
     }
 
-    return wantarray ? ($svk, \$output) : $svk;
+    $obj->{svk_handle} = $svk;
+    return $svk;
 }
 
 1;
